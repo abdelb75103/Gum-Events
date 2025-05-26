2345773f1d7ed2129d7e624e86fc89ebc24698f4
Abdel Babiker <abdelbabiker@gmail.com>
2025-05-26 02:06:10 +0000

    sahde the area under the curve

--- src/components/sections/our-impact-section.tsx
+++ src/components/sections/our-impact-section.tsx
@@ -104,8 +104,8 @@ export default function OurImpactSection() {
                 >
                   <defs>
                     <linearGradient id="fillMembers" x1="0" y1="0" x2="0" y2="1">
-                      <stop offset="5%" stopColor="var(--color-members)" stopOpacity={0.7}/>
-                      <stop offset="95%" stopColor="var(--color-members)" stopOpacity={0.2}/>
+                      <stop offset="5%" stopColor="var(--color-members)" stopOpacity={0.8}/>
+                      <stop offset="95%" stopColor="var(--color-members)" stopOpacity={0.3}/>
                     </linearGradient>
                   </defs>
                   <CartesianGrid strokeDasharray="3 3" vertical={false} strokeOpacity={0.5} />
@@ -158,4 +158,3 @@ export default function OurImpactSection() {
     </section>
   );
 }
-
